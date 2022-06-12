c6056fc85f1833415044504f01e676333ca896a0
Macy Kay Mannix <macymannix@gmail.com>
2022-06-12 15:39:07 -0700

    routes

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: '',
+  password: 'lol',
   database: 'election'
 });
 
